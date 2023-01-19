fe013a52cb337ebe91026424ae1b7e1ba413cc46
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2023-01-19 13:19:59 +0200

    added connection/disconnection server logging

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
+const io = require("socket.io")(http);
 
+io.on("connection", (socket) => {
+    console.log("user connected");
+    
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
+});
 
 http.listen(port, () => {
     console.log("server running at port "+port)
