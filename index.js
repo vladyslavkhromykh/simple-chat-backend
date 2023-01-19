2490c5c6c692775a3fb69eab9c936760ecacdc3d
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2023-01-19 12:47:50 +0200

    replaced single-quoted with double-quotes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const app = require("express")();
+const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 
 
 http.listen(port, () => {
-    console.log('server running at port ${port}')
+    console.log("server running at port "+port)
 })
