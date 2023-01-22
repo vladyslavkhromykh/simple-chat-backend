721f198e5839726cc86cfb0064d1b140e37d20a1
Vladyslav Khromykh <hromyhvladislav@gmail.com>
2023-01-22 21:18:04 +0200

    added history writing into file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(http);
+const fs = require("fs");
 
 io.on("connection", (socket) => {
     console.log("user connected");
@@ -12,10 +13,34 @@ io.on("connection", (socket) => {
 
     socket.on("message", message => {
         console.log("message:"+message);
+
+        let historyFile = "history.txt"
+
+        console.log("\n" + historyFile+ " before writing:",
+        fs.readFileSync(historyFile, "utf8"));
+
+        fs.appendFile(historyFile, "\n" + message, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("\n"+historyFile, "utf8");
+            }
+        });
         io.emit("message", message);
     })
 });
 
 http.listen(port, () => {
-    console.log("server running at port "+port)
+    console.log("server running at port "+port);
+
+    let historyFile = "history.txt"
+    if (!fs.existsSync(historyFile)) {
+        console.log(historyFile + "doesn't exists so we are about to create one")
+        fs.open(historyFile, 'w', function(err, file) {
+            if (err) throw err;
+            console.log(historyFile + " created");
+        });
+    } else {
+        console.log(historyFile + " already exists");
+    }
 })
